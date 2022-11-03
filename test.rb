c92e472c55d46249b75e7ad0fd6e3abc1ec5dbcd
robinbenitezmora <robinbenitez1990@gmail.com>
2022-11-03 11:22:23 -0500

    Create a 'test.rb' file

---
+++ test.rb
@@ -0,0 +1,14 @@
+require_relative './list.rb'
+
+list = MyList.new(1, 2, 3, 4)
+
+# Test 1
+list.all? { |e| e < 5 } # => true
+list.all? { |e| e > 5 } # => false
+
+# Test 2
+list.any? { |e| e == 2 } # => true
+list.any? { |e| e == 5 } # => false
+
+# Test 3
+list.filter(&:even?) # => [2, 4]
